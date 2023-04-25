d474a7ba1aeab525340a18e243ab5a9f9f9c9e0d
patelvraj18 <p.vraj@wustl.edu>
2023-04-25 17:31:38 -0500

    finished ui of discover

--- Components/Discover.js
+++ Components/Discover.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, TextInput, View, Keyboard, Button, ScrollView, Text } from "react-native";
+import { StyleSheet, TextInput, View, Keyboard, Button, ScrollView, Text, Image } from "react-native";
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import { useState, useContext, useEffect } from 'react';
 import * as db_operations from '../db_operations.js';
@@ -16,7 +16,7 @@ const Discover = ({ route, navigation }) => {
       db_operations.getUsers().then((users) => {
         console.log(users)
         usernames = []
-        for (user of Object.values(users)){
+        for (user of Object.values(users)) {
           usernames.push(user)
         }
         console.log(usernames)
@@ -62,17 +62,23 @@ const Discover = ({ route, navigation }) => {
           return (
             <View style={styles.userContainer}>
               <TouchableOpacity onPress={() => navigation.navigate(
-              'OtherProfile', {
+                'OtherProfile', {
                 username: user.username,
                 current_username: username,
                 isDefaultUser: false,
               }
               )}>
-                <Text style={styles.username}>{user.username}</Text>
+                <View style={styles.containerMain}>
+                  <View style={styles.profContainer}>
+                    <Image source={require('../assets/images/dog_picture.jpg')}
+                      style={styles.prof} />
+                  </View>
+                  <Text style={styles.username}>{user.username}</Text>
+                </View>
               </TouchableOpacity>
             </View>
-            )
-          })
+          )
+        })
         }
       </ScrollView>
     </View>
@@ -101,10 +107,32 @@ const styles = StyleSheet.create({
     marginTop: 4,
   },
   input: {
-    fontFamily: 'InriaSans-Regular',
+    // fontFamily: 'InriaSans-Regular',
     color: '#7C7C7C',
     fontSize: 14,
-  }
+  },
+  userContainer: {
+    marginLeft: 40,
+    marginTop: 30,
+  },
+  prof: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  profContainer: {
+    marginRight: 10,
+  },
+  containerMain: {
+    flexDirection: 'row',
+  },
+  username: {
+    // fontFamily: 'InriaSans-Bold',
+    color: '#7C7C7C',
+    fontSize: 14,
+    marginTop: 5,
+
+  },
 });
 
 export default Discover;
